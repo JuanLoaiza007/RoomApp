268ba2c5997514a724bb09a483996b4a7897dab1
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-23 14:40:12 -0500

    refactor: move MainActivity to view package

--- app/src/main/java/com/ihuntgore/roomapp/view/MainActivity.kt
+++ app/src/main/java/com/ihuntgore/roomapp/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ihuntgore.roomapp
+package com.ihuntgore.roomapp.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ihuntgore.roomapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
